052c9ddb111396770c2d5a93c098a780325162e9
leihao <leihao@xiaoduotech.com>
2020-09-29 15:43:04 +0800

    zzz

---
+++ precache-manifest.bc6afbef8228fa6efa4310bbf5656395.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e215a100fef9681acecdf4e56be97f92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36a6ce352c3fe99ad71c",
+    "url": "/static/css/main.7d6cce86.chunk.css"
+  },
+  {
+    "revision": "7fae10c6edf74c216ff8",
+    "url": "/static/js/2.b6ae2e87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b6ae2e87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36a6ce352c3fe99ad71c",
+    "url": "/static/js/main.998dc4b8.chunk.js"
+  },
+  {
+    "revision": "eef1400e6104db2ea575",
+    "url": "/static/js/runtime-main.f7f2aae3.js"
+  }
+]);
